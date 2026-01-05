4a20262456226232c9dde538d56dfb868e9dd676
Shravz08 <shravanimujumale@gmail.com>
2026-01-05 13:31:35 +0530

    Add files via upload

---
+++ control1.py
@@ -0,0 +1,64 @@
+#que2
+# i = 1
+# while i <= 100:
+#     if i % 7 == 0: 
+#         print("Found",i)
+#         break
+#     i += 1
+
+#que3
+# i = int(input("Enter the number:"))
+# #for i in range(-4,2):
+# if i <= 0:
+#         pass
+# elif i >= 0:
+#     print("Negative")
+# else:
+#     print("Zero")
+
+#que4
+# for i in range(0,101):
+#     if i % 5== 0 and i % 7 ==0 :
+#         print(i)
+#         break
+
+#que5
+# l=[12,32,43,300,4,34,23,50,123,545,67]
+# # for i in l:
+# #     if i > 50:
+# #         print(i)
+# #         break
+# i = 0
+# while i <= len(l):
+#     if l[i] > 50:
+#         print(l[i])
+#         break
+#     i += 1
+# #que6
+# for i in range (1, 21):
+#     if i % 2 == 0:
+#         pass
+#     else:
+#         print(i)
+
+# #que7
+# for i in range(0,31):
+#     if i % 2 == 0:
+#         pass
+#     else:
+#         print(i)
+
+#que8
+# lis=[-3,56,3,2,-4,-6,6,-44,43,21,23,55,-8]
+# for i in lis:
+#    if i < 0:
+#       continue
+#    print(i)
+
+#que9
+i = 0
+while i < 100: 
+   if i.isprime():
+   # if i % i == 0 and i % 1 == i:
+      print(i)
+      i += 1
